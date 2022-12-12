779662a82549433e58042b3eb0ac855c8ee1cec7
mduclos68 <mduclos@sfu.ca>
2022-12-11 18:40:42 -0800

    fix helper

--- helper.c
+++ helper.c
@@ -2,54 +2,6 @@
 // Helper functions used by multiple modules
 #include "helper.h"
 
-
-// Helper function that puts the system to slep for the specified number of Ms
-void sleepForMs(long long delayInMs){
-    const long long NS_PER_MS = 1000 * 1000;
-    const long long NS_PER_SECOND = 1000000000;
-    long long delayNs = delayInMs * NS_PER_MS;
-    int seconds = delayNs / NS_PER_SECOND;
-    int nanoseconds = delayNs % NS_PER_SECOND;
-    struct timespec reqDelay = {seconds, nanoseconds};
-    nanosleep(&reqDelay, (struct timespec *) NULL);
-}
-
-
-// Helper function that gets the current time in Ms
-long long getTimeInMs(void){
-    struct timespec spec;
-    clock_gettime(CLOCK_REALTIME, &spec);
-    long long seconds = spec.tv_sec;
-    long long nanoSeconds = spec.tv_nsec;
-    long long milliSeconds = seconds * 1000 + nanoSeconds / 1000000;
-    return milliSeconds;
-}
-
-
-// Helper function that runs a given system command
-void Helper_runCommand(char* command){
-    // Execute the shell command (output into pipe)
-    
-    FILE *pipe = popen(command, "r");
-
-    // Ignore output of the command; but consume it 
-    // so we don't get an error when closing the pipe.
-    
-    char buffer[1024];
-    while (!feof(pipe) && !ferror(pipe)) {
-        if (fgets(buffer, sizeof(buffer), pipe) == NULL)
-        break;
-    }
-
-    // Get the exit code from the pipe; non-zero is an error:
-    int exitCode = WEXITSTATUS(pclose(pipe));
-    if (exitCode != 0) {
-        perror("Unable to execute command:");
-        printf(" command: %s\n", command);
-        printf(" exit code: %d\n", exitCode);
-    }
-}
-
 // Helper function that reads from a file specified 
 int Helper_readFromFile(char *fileName){
 
@@ -89,3 +41,50 @@ void Helper_writeToFile(char *fileName, char* message){
     pFile = NULL;
     Helper_sleepForMs(300); // sleep for 300ms
 }
+
+// Helper function that runs a given system command
+void Helper_runCommand(char* command){
+    // Execute the shell command (output into pipe)
+    
+    FILE *pipe = popen(command, "r");
+
+    // Ignore output of the command; but consume it 
+    // so we don't get an error when closing the pipe.
+    
+    char buffer[1024];
+    while (!feof(pipe) && !ferror(pipe)) {
+        if (fgets(buffer, sizeof(buffer), pipe) == NULL)
+        break;
+    }
+
+    // Get the exit code from the pipe; non-zero is an error:
+    int exitCode = WEXITSTATUS(pclose(pipe));
+    if (exitCode != 0) {
+        perror("Unable to execute command:");
+        printf(" command: %s\n", command);
+        printf(" exit code: %d\n", exitCode);
+    }
+}
+
+// Helper function that puts the system to slep for the specified number of Ms
+void Helper_sleepForMs(long long delayInMs){
+    const long long NS_PER_MS = 1000 * 1000;
+    const long long NS_PER_SECOND = 1000000000;
+    long long delayNs = delayInMs * NS_PER_MS;
+    int seconds = delayNs / NS_PER_SECOND;
+    int nanoseconds = delayNs % NS_PER_SECOND;
+    struct timespec reqDelay = {seconds, nanoseconds};
+    nanosleep(&reqDelay, (struct timespec *) NULL);
+}
+
+
+// Helper function that gets the current time in Ms
+long long getTimeInMs(void){
+    struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    long long seconds = spec.tv_sec;
+    long long nanoSeconds = spec.tv_nsec;
+    long long milliSeconds = seconds * 1000 + nanoSeconds / 1000000;
+    return milliSeconds;
+}
+
--- helper.h
+++ helper.h
@@ -1,5 +1,11 @@
-// helper.h
-// Description: Contains helper functions used by multiple modules
+/*
+* Description: Contains miscellaneous helper functions used by multiple modules
+* Created By: Kaleigh Toering & Laura Flood
+* Contributors: Megan Duclos
+* Originally Created For: ENSC 351 Assignment 1
+* Sources Used: ENSC 351 GPIO guide and Assignment 1
+*/
+
 #ifndef HELPER_H
 #define HELPER_H
 
@@ -10,16 +16,18 @@
 #include <time.h>
 
 
-// Helper function that runs a given system command
-void sleepForMs(long long delayInMs);
-
 // Helper function that gets the current time in Ms
 long long getTimeInMs(void);
-
-void Helper_runCommand(char* command);
 
 // Helper function that reads from a file specified 
 int Helper_readFromFile(char *fileName);
 
 void Helper_writeToFile(char *fileName, char* message);
+
+// Helper function that runs a given system command
+void Helper_runCommand(char* command);
+
+// Helper function that runs a given system command
+void Helper_sleepForMs(long long delayInMs);
+
 #endif
