3d708dd89783c02ed4a9f162f757d549d4c6aca9
mduclos68 <mduclos@sfu.ca>
2022-12-14 17:24:18 -0800

    Update main.c

--- main.c
+++ main.c
@@ -23,8 +23,6 @@
 #define TASK2_AUDIO "colour-wav-files/Message_Jeu_2.wav"
 #define TASK3_AUDIO "colour-wav-files/Message_Jeu_3.wav"
 
-static wavedata_t *pSound;
-
 
 int main()
 {    
@@ -35,16 +33,18 @@ int main()
 
     /*********** First Challenge ***********/
     // read opening message
+    wavedata_t *pSound;
     AudioMixer_readWaveFileIntoMemory(START_MESSAGE_AUDIO, pSound);
     AudioMixer_queueSound(pSound);
     Helper_sleepForMs(SPEECH_LENGTH_START);
     AudioMixer_freeWaveFileData(pSound);
 
     // Read first task
-    AudioMixer_readWaveFileIntoMemory(TASK1_AUDIO, pSound);
-    AudioMixer_queueSound(pSound);
+    wavedata_t *pSound1;
+    AudioMixer_readWaveFileIntoMemory(TASK1_AUDIO, pSound1);
+    AudioMixer_queueSound(pSound1);
     Helper_sleepForMs(SPEECH_LENGHT_TASK_1);
-    AudioMixer_freeWaveFileData(pSound);
+    AudioMixer_freeWaveFileData(pSound1);
 
     TaskGenerator1_init();
 
@@ -60,10 +60,11 @@ int main()
 
     /*********** Second Challenge ***********/
     // Read second task
-    AudioMixer_readWaveFileIntoMemory(TASK2_AUDIO, pSound);
-    AudioMixer_queueSound(pSound);
+    wavedata_t *pSound2;
+    AudioMixer_readWaveFileIntoMemory(TASK2_AUDIO, pSound2);
+    AudioMixer_queueSound(pSound2);
     Helper_sleepForMs(SPEECH_LENGHT_TASK_2);
-    AudioMixer_freeWaveFileData(pSound);
+    AudioMixer_freeWaveFileData(pSound2);
 
     // Start second task
     TaskGenerator2_init();
@@ -82,10 +83,11 @@ int main()
 
     /*********** Third Challenge ***********/
     // Read third task
-    AudioMixer_readWaveFileIntoMemory(TASK3_AUDIO, pSound);
-    AudioMixer_queueSound(pSound);
+    wavedata_t *pSound3;
+    AudioMixer_readWaveFileIntoMemory(TASK3_AUDIO, pSound3);
+    AudioMixer_queueSound(pSound3);
     Helper_sleepForMs(SPEECH_LENGHT_TASK_3);
-    AudioMixer_freeWaveFileData(pSound);
+    AudioMixer_freeWaveFileData(pSound3);
 
     /*// Start second task
     TaskGenerator3_init();
