119f44f05692ed98a913efc97734a59ea4c26d87
mduclos68 <mduclos@sfu.ca>
2022-12-14 17:26:56 -0800

    Update main.c

--- main.c
+++ main.c
@@ -34,18 +34,18 @@ int main()
 
     /*********** First Challenge ***********/
     // read opening message
-    wavedata_t *pSound;
+    wavedata_t pSound;
     AudioMixer_readWaveFileIntoMemory(START_MESSAGE_AUDIO, pSound);
     AudioMixer_queueSound(pSound);
     Helper_sleepForMs(SPEECH_LENGTH_START);
     AudioMixer_freeWaveFileData(pSound);
 
     // Read first task
-    wavedata_t *pSound1;
-    AudioMixer_readWaveFileIntoMemory(TASK1_AUDIO, pSound1);
-    AudioMixer_queueSound(pSound1);
+    //wavedata_t *pSound1;
+    AudioMixer_readWaveFileIntoMemory(TASK1_AUDIO, pSound);
+    AudioMixer_queueSound(pSound);
     Helper_sleepForMs(SPEECH_LENGHT_TASK_1);
-    AudioMixer_freeWaveFileData(pSound1);
+    AudioMixer_freeWaveFileData(pSound);
 
     TaskGenerator1_init();
 
@@ -61,11 +61,11 @@ int main()
 
     /*********** Second Challenge ***********/
     // Read second task
-    wavedata_t *pSound2;
-    AudioMixer_readWaveFileIntoMemory(TASK2_AUDIO, pSound2);
-    AudioMixer_queueSound(pSound2);
+    //wavedata_t *pSound2;
+    AudioMixer_readWaveFileIntoMemory(TASK2_AUDIO, pSound);
+    AudioMixer_queueSound(pSound);
     Helper_sleepForMs(SPEECH_LENGHT_TASK_2);
-    AudioMixer_freeWaveFileData(pSound2);
+    AudioMixer_freeWaveFileData(pSound);
 
     // Start second task
     TaskGenerator2_init();
@@ -84,11 +84,11 @@ int main()
 
     /*********** Third Challenge ***********/
     // Read third task
-    wavedata_t *pSound3;
-    AudioMixer_readWaveFileIntoMemory(TASK3_AUDIO, pSound3);
-    AudioMixer_queueSound(pSound3);
+    //wavedata_t *pSound3;
+    AudioMixer_readWaveFileIntoMemory(TASK3_AUDIO, pSound);
+    AudioMixer_queueSound(pSound);
     Helper_sleepForMs(SPEECH_LENGHT_TASK_3);
-    AudioMixer_freeWaveFileData(pSound3);
+    AudioMixer_freeWaveFileData(pSound);
 
     /*// Start second task
     TaskGenerator3_init();
